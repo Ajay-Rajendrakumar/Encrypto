5a274fb0be2e7c1de217a00251ebffe771bf1434
Ajay-Rajendrakumar <ajayrajendrakumar06@gmail.com>
2021-10-15 05:52:43 +0530

    Latest Commit

--- be_backend/firebase_connector.py
+++ be_backend/firebase_connector.py
@@ -7,13 +7,7 @@ default_app = initialize_app(cred ,{
 })
 db = firestore.client()
 firebaseConfig = {
-  "apiKey": "AIzaSyAv8KDEpHtefofUAdTxiVPGiIBHlY-a1BQ",
-  "authDomain": "billencryptor.firebaseapp.com",
-  "databaseURL": "https://billencryptor-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "billencryptor",
-  "storageBucket": "billencryptor.appspot.com",
-  "messagingSenderId": "824226648084",
-  "appId": "1:824226648084:web:28be2e8a369e2757339701"
+
 }
 
 firebase=pyrebase.initialize_app(firebaseConfig)
@@ -23,19 +17,3 @@ def getConnection():
 def getStorage():
     return firebase.storage()
 
-
-
-
-#    todo_ref = db.collection('user_info')
-# file_contents={
-#     "id":"1",
-#     "name":"Ajay",
-#     "password":"543234"
-# }
-# id = file_contents['id']
-# todo_ref.document(id).set(file_contents)
-
-#todo = todo_ref.document("1").get()
-#print(todo.to_dict())
-
-#todo_ref.document(id).update(request.json)
